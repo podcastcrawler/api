316e38e699a4b1e039a88c0de4e48f73de4bf2d7
Dorian Neto <doriansampaioneto@gmail.com>
2017-07-15 01:04:59 -0300

    test(feature): test for not found result on get method

--- tests/PodcastCrawlerTest.php
+++ tests/PodcastCrawlerTest.php
@@ -6,17 +6,31 @@ class PodcastCrawlerTest extends TestCase
     {
         $response = $this->call('GET', '/list', ['name' => 'nerdcast']);
 
+        $data = $response->original;
+
         $this->assertEquals(200, $response->status());
+        $this->assertGreaterThanOrEqual(1, count($data['result_count']));
+        $this->assertGreaterThanOrEqual(1, count($data['podcasts']));
     }
 
     public function testGetByNameWithLimit()
     {
-        $response = $this->call('GET', '/list', ['name' => 'nerdcast', 'limit' => 4]);
+        $response = $this->call('GET', '/list', ['name' => 'ouvo', 'limit' => 4]);
 
         $data = $response->original;
 
         $this->assertEquals(200, $response->status());
-        $this->assertEquals(4, $data['result_count']);
-        $this->assertCount(4, $data['podcasts']);
+        $this->assertLessThanOrEqual(4, $data['result_count']);
+        $this->assertLessThanOrEqual(4, count($data['podcasts']));
+    }
+
+    public function testGetByNameNotFound()
+    {
+        $response = $this->call('GET', '/list', ['name' => 'gjkhjgk']);
+
+        $data = $response->original;
+
+        $this->assertEquals(404, $response->status());
+        $this->assertEquals(0, $data['result_count']);
     }
 }
